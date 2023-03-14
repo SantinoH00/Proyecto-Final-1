c88233987813f6597aa980f3b9d80fef269d3aa6
Santino Hernandez <hernandezsantino6@gmail.com>
2023-03-14 01:27:05 -0300

    primer proyecto

---
+++ Funciones.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function(){
+    console.log('capturamos el evento click');
+    document.getElementById("demo").innerHTML = "Practicar artes marciales, yoga, tocar la guitarra y salir con amigos."
+}
